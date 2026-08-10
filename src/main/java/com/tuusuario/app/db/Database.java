a98e344a5c6678d7ba7de7f542a03aad58c0274c
Nicheel <lomashernandezfernandoalberto@gmail.com>
2026-08-10 13:32:03 -0600

    Agregando Archivos

---
+++ src/main/java/com/tuusuario/app/db/Database.java
@@ -0,0 +1,5 @@
+package com.tuusuario.app.db;
+
+public class Database {
+    
+}
---
+++ src/test/java/com/tuusuario/app/dao/UserDao.java
@@ -0,0 +1,11 @@
+package com.tuusuario.app.dao;
+
+import com.tuusuario.app.model.User;
+
+public class UserDao {
+
+    public void insert(User usuario){
+
+    }
+
+}
